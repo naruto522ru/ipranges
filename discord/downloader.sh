d56d6f6109dd67a7327ad2adbb8bd2b656c1bcff
IA <itachi522ru@gmail.com>
2024-10-12 15:18:05 +0300

    Edit script download for discord for fast processing

--- discord/downloader.sh
+++ discord/downloader.sh
@@ -32,25 +32,27 @@ curl --max-time 180 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercont
 dos2unix discord/domain.txt
 cat discord/domain.txt | sed '/.solutions/d' | sed '/airhorn/d' | sed '/anime/d' | uniq | sort -u | sponge discord/domain.txt && sed -i '/^$/d' discord/domain.txt
 
-# resolving
-# A-Record (IPv4)
 rm -f /tmp/discord_resolve.txt;
-cat discord/domain.txt | utils/mdig-bolvan --family=4 --threads=$(nproc) | sed '/[A-Za-z]/d' | sed 's/ //g' | sed '/^$/d' | sed '/^0.0.0.0/d' | sed '/^127.0.0.1/d' | sed '/^1.1.1.1/d' | sed '/^8.8.8.8/d' | sed '/^1.0.0.1/d' | sed '/^8.8.4.4/d' | grepcidr -v '0.0.0.0/8' | grepcidr -v '0.0.0.0/32' | grepcidr -v '10.0.0.0/8' | grepcidr -v '100.64.0.0/10' | grepcidr -v '127.0.0.0/8' | grepcidr -v '169.254.0.0/16' | grepcidr -v '172.16.0.0/12' | grepcidr -v '192.0.0.0/24' | grepcidr -v '192.0.0.0/29' | grepcidr -v '192.0.0.170/32' | grepcidr -v '192.0.0.171/32' | grepcidr -v '192.0.2.0/24' | grepcidr -v '192.88.99.0/24' | grepcidr -v '192.88.99.1/32' | grepcidr -v '192.168.0.0/16' | grepcidr -v '198.51.100.0/24' | grepcidr -v '198.18.0.0/15' | grepcidr -v '203.0.113.0/24' | grepcidr -v '224.0.0.0/4' | grepcidr -v '240.0.0.0/4' | grepcidr -v '255.255.255.255/32' >> /tmp/discord_resolve.txt
+# resolving
+# Both Record (IPv4/IPv6)
+cat discord/domain.txt | utils/mdig-bolvan --threads=$(nproc) >> /tmp/discord_resolve.txt
+# A-Record (IPv4)
+#cat discord/domain.txt | utils/mdig-bolvan --family=4 --threads=$(nproc) >> /tmp/discord_resolve.txt
 # AAAA-Record (IPv6)
 #cat discord/domain.txt | utils/mdig-bolvan --family=6 --threads=$(nproc) >> /tmp/discord_resolve.txt
 
 # ip's
 rm -f /tmp/ips_discord.txt /tmp/voice_server1.txt /tmp/voice_server2.txt;
 curl --max-time 180 --retry-delay 3 --retry 10 -4s -# https://gist.githubusercontent.com/AndyIsHereBoi/bf57d7fa1661c82b4a7f5987e56420bf/raw/7ef7bc308d36bcd1ad5aff42de6c43c838873563/as%2520of%25209-2-2024 > /tmp/voice_server1.txt
-cat /tmp/voice_server1.txt | grep -vEe '(ip|city|region|country|org)' | sed 's/,//g' | sed 's/{//g' | sed 's/}//g' | sed 's/"//g' | sed 's/]//g' | sed 's/dns://g' | sed '/:\[/d' | sed 's/ //g' | grepcidr -v '0.0.0.0/8' | grepcidr -v '0.0.0.0/32' | grepcidr -v '10.0.0.0/8' | grepcidr -v '100.64.0.0/10' | grepcidr -v '127.0.0.0/8' | grepcidr -v '169.254.0.0/16' | grepcidr -v '172.16.0.0/12' | grepcidr -v '192.0.0.0/24' | grepcidr -v '192.0.0.0/29' | grepcidr -v '192.0.0.170/32' | grepcidr -v '192.0.0.171/32' | grepcidr -v '192.0.2.0/24' | grepcidr -v '192.88.99.0/24' | grepcidr -v '192.88.99.1/32' | grepcidr -v '192.168.0.0/16' | grepcidr -v '198.51.100.0/24' | grepcidr -v '198.18.0.0/15' | grepcidr -v '203.0.113.0/24' | grepcidr -v '224.0.0.0/4' | grepcidr -v '240.0.0.0/4' | grepcidr -v '255.255.255.255/32' | uniq | sort -u > /tmp/ips_discord.txt
+cat /tmp/voice_server1.txt | grep -vEe '(ip|city|region|country|org)' | sed 's/,//g' | sed 's/{//g' | sed 's/}//g' | sed 's/"//g' | sed 's/]//g' | sed 's/dns://g' | sed '/:\[/d' | sed 's/ //g'   | uniq | sort -u > /tmp/ips_discord.txt
 curl --max-time 180 --retry-delay 3 --retry 10 -4s -# https://raw.githubusercontent.com/GhostRooter0953/discord-voice-ips/master/discord-voice-ip-list > /tmp/voice_server2.txt
 #https://raw.githubusercontent.com/GhostRooter0953/discord-voice-ips/refs/heads/master/discord-voice-ip-list
-cat /tmp/voice_server2.txt | grepcidr -v '0.0.0.0/8' | grepcidr -v '0.0.0.0/32' | grepcidr -v '10.0.0.0/8' | grepcidr -v '100.64.0.0/10' | grepcidr -v '127.0.0.0/8' | grepcidr -v '169.254.0.0/16' | grepcidr -v '172.16.0.0/12' | grepcidr -v '192.0.0.0/24' | grepcidr -v '192.0.0.0/29' | grepcidr -v '192.0.0.170/32' | grepcidr -v '192.0.0.171/32' | grepcidr -v '192.0.2.0/24' | grepcidr -v '192.88.99.0/24' | grepcidr -v '192.88.99.1/32' | grepcidr -v '192.168.0.0/16' | grepcidr -v '198.51.100.0/24' | grepcidr -v '198.18.0.0/15' | grepcidr -v '203.0.113.0/24' | grepcidr -v '224.0.0.0/4' | grepcidr -v '240.0.0.0/4' | grepcidr -v '255.255.255.255/32' | uniq | sort -u >> /tmp/ips_discord.txt
+cat /tmp/voice_server2.txt | uniq | sort -u >> /tmp/ips_discord.txt
 dos2unix /tmp/ips_discord.txt
 cat /tmp/ips_discord.txt /tmp/discord_resolve.txt | uniq | sort -u > /tmp/discord.txt
 
 # save ipv4
-grep -v ':' /tmp/discord.txt | grep -v "0.0.0.0" > /tmp/discord-ipv4.txt
+grep -v ':' /tmp/discord.txt | sed '/[A-Za-z]/d' | sed 's/ //g' | sed '/^$/d' | sed '/^0.0.0.0/d' | sed '/^127.0.0.1/d' | sed '/^1.1.1.1/d' | sed '/^8.8.8.8/d' | sed '/^1.0.0.1/d' | sed '/^8.8.4.4/d' | grepcidr -v '0.0.0.0/8' | grepcidr -v '0.0.0.0/32' | grepcidr -v '10.0.0.0/8' | grepcidr -v '100.64.0.0/10' | grepcidr -v '127.0.0.0/8' | grepcidr -v '169.254.0.0/16' | grepcidr -v '172.16.0.0/12' | grepcidr -v '192.0.0.0/24' | grepcidr -v '192.0.0.0/29' | grepcidr -v '192.0.0.170/32' | grepcidr -v '192.0.0.171/32' | grepcidr -v '192.0.2.0/24' | grepcidr -v '192.88.99.0/24' | grepcidr -v '192.88.99.1/32' | grepcidr -v '192.168.0.0/16' | grepcidr -v '198.51.100.0/24' | grepcidr -v '198.18.0.0/15' | grepcidr -v '203.0.113.0/24' | grepcidr -v '224.0.0.0/4' | grepcidr -v '240.0.0.0/4' | grepcidr -v '255.255.255.255/32' > /tmp/discord-ipv4.txt
 
 # save ipv6
 grep ':' /tmp/discord.txt > /tmp/discord-ipv6.txt
